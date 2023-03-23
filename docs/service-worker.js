74defee7fa10a27ba318c3a0616471639f29fff5
HadrienVillani <33479968+HadrienVillani@users.noreply.github.com>
2023-03-23 13:00:49 +0100

    first commit

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Cantaloube/precache-manifest.227635e59e9c76e0a99d12f6cb119813.js"
+  "/Cantaloube/precache-manifest.d81dc95d88615773c76e1aaa56a17bcf.js"
 );
 
 workbox.clientsClaim();
